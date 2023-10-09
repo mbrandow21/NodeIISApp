f45bd2de6539c52c1285dcc7034bd4adb160ac3a
jblackman42 <jblackman@pureheart.org>
2023-10-09 16:44:19 -0700

    test deploy 2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const navigation = express.Router();
 
 //home page
 navigation.get('/', (req, res) => {
-    res.send('Hello Test')
+    res.send('Hello World Test')
 })
 navigation.get('/console', (req, res) => {
     res.render('pages/live-console')
